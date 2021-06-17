b76e36bf40c0efa970150fe432f6b585b8730b3f
Alan <180203813@aston.ac.uk>
2021-06-17 09:34:13 +0100

    rid comment

--- Assets/Scripts/BlockScripts/nextLetter.cs
+++ Assets/Scripts/BlockScripts/nextLetter.cs
@@ -19,7 +19,6 @@ public class NextLetter : MonoBehaviour
 
         // totalWeight is the sum of all alphabet' weight
 
-        //int randomNumber = _rnd.Next(0, totalWeight);
         int randomNumber = Random.Range(0, totalWeight);
 
         string selectedLetter = null;
