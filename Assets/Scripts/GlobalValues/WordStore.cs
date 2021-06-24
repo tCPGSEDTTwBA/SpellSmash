3b30aac4e57166a7143b99547c95f84addbe4dd6
Matt Binnie <mattbinnie1@icloud.com>
2021-06-24 17:12:03 +0100

    Fixed

--- Assets/Scripts/GlobalValues/WordStore.cs
+++ Assets/Scripts/GlobalValues/WordStore.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 static class WordStore
 {
     private static List<string> WORDS;
-    private static readonly TextAsset WORDTEXTFILE = Resources.Load<TextAsset>("WordStore/WordList");
-    private static readonly TextAsset FRENCHWORDTEXTFILE = Resources.Load<TextAsset>("WordStore/FrenchWordList");
+    private static readonly string PATH_PREFIX = "WordStore/WordList_";
 
     public static void GenerateWords(string language)
     {
-        if(language == "English")
-        {
-            WORDS = new List<string>(WORDTEXTFILE.text.Replace("\r\n", "\n").Split('\n'));
-        } else
-        {
-            WORDS = new List<string>(FRENCHWORDTEXTFILE.text.Replace("\r\n", "\n").Split('\n'));
-        }
+        var filePath = PATH_PREFIX + language;
+        TextAsset WordList = Resources.Load<TextAsset>(filePath);
+        WORDS = new List<string>(WordList.text.Replace("\r\n", "\n").Split('\n'));
     }
 
     public static List<string> GetWords()
